cf85b7f9f08c737318d1a7077456d26167c1e2df
jackferno <44953676+jackferno@users.noreply.github.com>
2024-08-04 19:29:45 -0600

    Add files via upload

---
+++ src/main/java/net/mcreator/test/init/TestModProcedures.java
@@ -0,0 +1,14 @@
+
+/*
+ *	MCreator note: This file will be REGENERATED on each build.
+ */
+package net.mcreator.test.init;
+
+import net.mcreator.test.procedures.DiamondApplePlayerFinishesUsingItemProcedure;
+
+@SuppressWarnings("InstantiationOfUtilityClass")
+public class TestModProcedures {
+	public static void load() {
+		new DiamondApplePlayerFinishesUsingItemProcedure();
+	}
+}
